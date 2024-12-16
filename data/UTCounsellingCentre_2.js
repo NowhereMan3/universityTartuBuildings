64cd62fe5b971fe2f6bf1a8df48883db353f2a09
NowhereMan3 <147972435+NowhereMan3@users.noreply.github.com>
2024-12-16 09:28:36 +0200

    Delete data directory

--- data/UTCounsellingCentre_2.js
+++
@@ -1,8 +0,0 @@
-var json_UTCounsellingCentre_2 = {
-"type": "FeatureCollection",
-"name": "UTCounsellingCentre_2",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": "1", "Address": "W. Struve tn 1", "Name": "UT Counselling Centre" }, "geometry": { "type": "Point", "coordinates": [ 26.720601810563167, 58.376657377382372 ] } }
-]
-}
